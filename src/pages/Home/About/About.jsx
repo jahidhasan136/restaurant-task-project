ac28f4fd9df7597b0a1b87d3533e49c8b7118237
jahidhasan136 <jh3495689@gmail.com>
2024-11-04 02:00:29 +0600

    add react tabs to about section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -4,6 +4,8 @@ import fastDeliveryImg from '/public/assets/about/delivery.svg'
 import absoluteImg from '/public/assets/about/absolute.svg'
 import pickupDeliveryImg from '/public/assets/about/pickup.svg'
 import bgImg from '/public/assets/about/bg_img.svg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
 
 const About = () => {
     return (
@@ -14,20 +16,37 @@ const About = () => {
                     <div>
                         <img className='bg-cover w-full h-full' src={aboutImg} alt="" />
                     </div>
-                    <div className="grid lg:gap-8 sm:gap-6">
-                        <div className="flex gap-4">
-                            <button className="px-4 py-[6px] bg-[#B52B1D] text-white text-sm">About</button>
-                            <button>Experience</button>
-                            <button>Contact</button>
-                        </div>
-                        <div className='grid lg:gap-4 sm:gap-3'>
+                    {/* react tabs */}
+                    <Tabs className="grid lg:gap-8 sm:gap-6">
+                        <TabList className="flex gap-4">
+                            <Tab className="px-4 py-[6px] text-sm cursor-pointer">About</Tab>
+                            <Tab className="px-4 py-[6px] text-sm cursor-pointer">Experience</Tab>
+                            <Tab className="px-4 py-[6px] text-sm cursor-pointer">Contact</Tab>
+                        </TabList>
+                        <TabPanel className='grid lg:gap-4 sm:gap-3'>
                             <h2 className="uppercase xl:text-[62px] sm:text-[40pxs] xl:leading-[62px] font-bold">
                                 Exceptional Culinary Experience And Delicious Food
                             </h2>
                             <p className='text-[#333333]'>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.
                             </p>
-                        </div>
+                        </TabPanel>
+                        <TabPanel className='grid lg:gap-4 sm:gap-3'>
+                            <h2 className="uppercase xl:text-[62px] sm:text-[40pxs] xl:leading-[62px] font-bold">
+                                Exceptional Culinary Experience And Delicious Food
+                            </h2>
+                            <p className='text-[#333333]'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.
+                            </p>
+                        </TabPanel>
+                        <TabPanel className='grid lg:gap-4 sm:gap-3'>
+                            <h2 className="uppercase xl:text-[62px] sm:text-[40pxs] xl:leading-[62px] font-bold">
+                                Exceptional Culinary Experience And Delicious Food
+                            </h2>
+                            <p className='text-[#333333]'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.
+                            </p>
+                        </TabPanel>
                         <div className="flex items-center gap-8">
                             <button className="btn">About More</button>
                             <div className='flex items-center gap-2'>
@@ -35,7 +54,7 @@ const About = () => {
                                 <p>+88 3426 739 485</p>
                             </div>
                         </div>
-                    </div>
+                    </Tabs>
                 </div>
                 <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-between lg:mt-[74px] sm:mt-[50px] sm:gap-8'>
                     <div className='flex gap-4 items-center'>
